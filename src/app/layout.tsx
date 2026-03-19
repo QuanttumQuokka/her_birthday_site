033ac84820eea88527723168e2998f93bc3de6d4
QuanttumQuokka <arushjain9557@gmail.com>
2026-03-19 23:57:02 +0530

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import MusicPlayer from "./MusicPlayer";
+
+export const metadata = {
+  title: "For You ❤️",
+  description: "Birthday Surprise",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+
+        {/* MUSIC COMPONENT */}
+        <MusicPlayer />
+
+        {children}
+      </body>
+    </html>
+  );
+}
